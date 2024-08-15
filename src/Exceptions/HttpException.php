1af6fea687d64ffaa27e339888d60a1a3ffe601a
Phil <phil@harmonie.media>
2024-08-15 09:05:37 +0200

    fix: handle string and array in HttpException

--- src/Exceptions/HttpException.php
+++ src/Exceptions/HttpException.php
@@ -10,16 +10,21 @@ class HttpException extends Exception
     // @codeCoverageIgnoreStart
     public function __construct($message = '', $code = 0, Throwable $previous = null)
     {
+        if (is_array($message)) {
+            $message = json_encode($message);
+        }
+
         parent::__construct($message, $code, $previous);
     }
 
     public static function new(string $url, int $status, string $message, $response): HttpException
     {
+        $responseMessage = is_array($response) ? json_encode($response) : $response;
+
         return new self(
-            $response,
             sprintf(
-                'Http request to %s failed with a status of %d and error message: %s',
-                $url, $status, $message
+                'Http request to %s failed with a status of %d and error message: %s. Response: %s',
+                $url, $status, $message, $responseMessage
             )
         );
     }
